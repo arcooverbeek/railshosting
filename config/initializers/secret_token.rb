c9d8b74c4987034ef7307b4a78f9d219ff747e5e
Arco Overbeek <arcooverbeek@gmail.com>
2013-04-25 15:52:38 +0200

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railshosting::Application.config.secret_token = '8034c3062a54b6dacf58dc04d486a6207b7ceb877e11ac2721fc524c5503d2a0673919544a895544ac0a506889ad264544e26ef6d2a80557ba9094f12fe19c29'
